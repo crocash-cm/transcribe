0906abb5d12ac47172987e9db5bae77a7064307e
crocash-cm <jashford.work@gmail.com>
2025-09-20 17:41:31 +0100

    Update with transcribe code, remove unnecessary sample-plugin code.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, setIcon } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,66 +16,59 @@ export default class TranscribePlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		let isTranscribing: boolean = false;
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		if (isTranscribing) {
+				// Stop transcribing
+				new Notice("Stopped transcribing.");
+				isTranscribing = false;
+				// Update icon and tooltip for start state
+				setIcon(ribbonIconEl, 'mic-off');
+				ribbonIconEl.setAttribute('aria-label', 'Start Listening');
+			} else {
+				// Start transcribing
+				new Notice("Started transcribing.");
+				isTranscribing = true;
+				// Update icon and tooltip for stop state
+				setIcon(ribbonIconEl, 'mic');
+				ribbonIconEl.setAttribute('aria-label', 'Stop Listening');
+			}
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		
+		// Start transcribing command
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'start-transcribing-command',
+			name: 'Start Transcribing',
 			callback: () => {
-				new TranscribeModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new TranscribeModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (!isTranscribing) {
+					// Start transcribing
+					new Notice("Started transcribing.");
+					isTranscribing = true;
+					// Update icon and tooltip for stop state
+					setIcon(ribbonIconEl, 'mic');
+					ribbonIconEl.setAttribute('aria-label', 'Stop Listening');
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new TranscribeSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Stop transcribing command
+		this.addCommand({
+			id: 'stop-transcribing-command',
+			name: 'Stop Transcribing',
+			callback: () => {
+				if (isTranscribing) {
+					// Stop transcribing
+					new Notice("Stopped transcribing.");
+					isTranscribing = false;
+					// Update icon and tooltip for start state
+					setIcon(ribbonIconEl, 'mic-off');
+					ribbonIconEl.setAttribute('aria-label', 'Start Listening');
+				}
+			}
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
